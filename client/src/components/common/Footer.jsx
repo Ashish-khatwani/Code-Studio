e42fbbe8d3d8f8db8828fd0aaa2583d8616d6e99
Ashish Khatwani <21it066@charusat.edu.in>
2024-05-07 10:46:05 +0530

    updated code studio

---
+++ client/src/components/common/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by{" "}
+                <a
+                    href="https://github.com/Ashish-Khatwani"
+                    className="text-primary underline underline-offset-1"
+                >
+                    Ashish Khatwani
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
